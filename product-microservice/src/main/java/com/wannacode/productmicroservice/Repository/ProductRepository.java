bb6749813e2642f8d25b4d5222a9b944dcf2d6b6
sergiojr01 <146067566+sergiojr01@users.noreply.github.com>
2024-05-09 21:08:25 +0200

    Delete product-microservice directory

--- product-microservice/src/main/java/com/wannacode/productmicroservice/Repository/ProductRepository.java
+++
@@ -1,8 +0,0 @@
-package com.wannacode.productmicroservice.Repository;
-
-import com.wannacode.productmicroservice.entity.ProductEntity;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface ProductRepository extends MongoRepository<ProductEntity, String> {
-
-}
